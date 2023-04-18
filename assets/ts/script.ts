535dfc8652631a8920a07a70d74a235abcaeefbe
Michael J. Shalewski <mshalewski@outlook.com>
2023-04-18 16:01:38 +0000

    added openweather api node library and submitted api key

---
+++ assets/ts/script.ts
@@ -0,0 +1,11 @@
+// API KEY: 93c71cac1fd4a57c64cd081205620eea
+import OpenWeatherAPI from "openweather-api-node";
+
+let weather = new OpenWeatherAPI({
+    key: "93c71cac1fd4a57c64cd081205620eea",
+    lang: "en",
+    locationName: "Milwaukee",
+    units: "imperial"
+});
+
+weather.getCurrent
